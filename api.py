7b995bb8245b8e7750d57cb4294c1aac2147089c
niranjan-gurung <gurung5130@gmail.com>
2025-03-20 15:40:10 +0000

    skills schema defined, and endpoint added.

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from flask_marshmallow import Marshmallow
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
 from marshmallow_sqlalchemy.fields import RelatedList, Nested
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+#from marshmallow import fields
 
 from dotenv import load_dotenv
 import os
@@ -36,6 +37,10 @@ schema (armour):
     - defense
     - resistances
 """
+
+"""
+  Models
+"""
 class ArmourModel(db.Model):
   id: Mapped[int] = mapped_column(primary_key=True)
   name: Mapped[str] = mapped_column(unique=False, nullable=False)
@@ -47,40 +52,80 @@ class ArmourModel(db.Model):
                                                   back_populates='armour')
   
   """TODO:"""
-  # skills: Mapped[list['SkillsModel']]
+  #skills: Mapped[list['SkillModel']] = relationship(back_populates='armour')
 
   def __repr__(self):
-    return '{}-{}-{}-{}-{}'\
+    return '{}-{}-{}-{}-{}' \
       .format(self.name, self.slug, self.type, self.rank, self.rarity)
+
 
 class SlotModel(db.Model):
   id: Mapped[int] = mapped_column(primary_key=True)
   level: Mapped[int] = mapped_column(nullable=True)
-  armour_id: Mapped[int] = mapped_column(db.ForeignKey('armour_model.id', 
-                                         ondelete='CASCADE'))
+  armour_id: Mapped[int] = mapped_column(db.ForeignKey('armour_model.id'))
   armour: Mapped['ArmourModel'] = relationship(back_populates='slots', 
-                                               foreign_keys=[armour_id])
+                                               foreign_keys=armour_id)
 
   def __repr__(self):
     return '{}-{}'.format(self.id, self.level)
   
-# class SkillsModel(db.Model):
-#   id: Mapped[int] = mapped_column(primary_key=True)
-#   name: Mapped[str] = mapped_column(unique=True, nullable=False)
-#   desc: Mapped[str] = mapped_column(nullable=False)
-#   level: Mapped[int] = mapped_column(nullable=True)
 
-#   def __repr__(self):
-#     return '{}'
+class SkillModel(db.Model):
+  id: Mapped[int] = mapped_column(primary_key=True)
+  name: Mapped[str] = mapped_column(unique=True, nullable=False)
+  type: Mapped[str] = mapped_column(nullable=False)   # armour / weapon
+  desc: Mapped[str] = mapped_column(nullable=False)
+  ranks: Mapped[list['SkillRankModel']] = relationship('SkillRankModel', 
+                                                        back_populates='skill')
   
-# class SkillTypeModel(db.Model):
-#   id: Mapped[int] = mapped_column(primary_key=True)
-#   name: Mapped[str] = mapped_column(unique=True, nullable=False)
-#   desc: Mapped[str] = mapped_column(nullable=False)
+  # armour_id: Mapped[int] = mapped_column(db.ForeignKey('armour_model.id'))
+  # armour: Mapped['ArmourModel'] = relationship(back_populates='skills', 
+  #                                              foreign_keys=armour_id)
 
-#   def __repr__(self):
-#     return '{}'
+  def __repr__(self):
+    return '{}-{}-{}-{}-{}' \
+      .format(self.id, self.name, self.type, self.desc, self.ranks)
   
+class SkillRankModel(db.Model):
+  id: Mapped[int] = mapped_column(primary_key=True)
+  level: Mapped[int] = mapped_column(nullable=False)  # skill level
+  desc: Mapped[str] = mapped_column(nullable=False)
+
+  skill_id: Mapped[int] = mapped_column(db.ForeignKey('skill_model.id'))
+  skill: Mapped['SkillModel'] = relationship(back_populates='ranks', 
+                                             foreign_keys=skill_id)
+  
+  def __repr__(self):
+    return '{}-{}-{}'.format(self.id, self.level, self.desc)
+  
+
+"""
+  Schema
+"""
+class SkillSchema(SQLAlchemyAutoSchema):
+  class Meta:
+    model = SkillModel
+    include_relationships = True
+    load_instance = True
+
+  id = auto_field()
+  name = auto_field()
+  type = auto_field()
+  desc = auto_field()
+  ranks = RelatedList(Nested('SkillRankSchema'))
+
+class SkillRankSchema(SQLAlchemyAutoSchema):
+  class Meta:
+    model = SkillRankModel
+    include_fk = True
+    load_instance = True
+  
+  id = auto_field()
+  level = auto_field()
+  skill_id = auto_field()
+  desc = auto_field()
+
+
 class ArmourSchema(SQLAlchemyAutoSchema):
   class Meta:
     model = ArmourModel
@@ -101,6 +146,10 @@ class SlotSchema(SQLAlchemyAutoSchema):
     include_fk = True
     load_instance = True
 
+
+"""
+  Validations
+"""
 armour_args = reqparse.RequestParser()
 armour_args.add_argument('name', type=str, required=True, help="Armour name can't be blank")
 armour_args.add_argument('slug', type=str, required=True, help="Armour slug can't be blank")
@@ -124,6 +173,25 @@ armour_fields = {
   'slots': fields.List(fields.Nested(slot_fields))
 }
 
+skill_rank_fields = {
+  'id': fields.Integer,
+  'level': fields.Integer,
+  'skill_id': fields.Integer,
+  'desc': fields.String
+}
+
+skills_fields = {
+  'id': fields.Integer,
+  'name': fields.String,
+  'type': fields.String,
+  'desc': fields.String,
+  'ranks': fields.List(fields.Nested(skill_rank_fields))
+}
+
+
+"""
+  Services
+"""
 class Armours(Resource):
   """get all armours OR single armour piece by id OR slug name"""
   @marshal_with(armour_fields)
@@ -183,6 +251,7 @@ class Armours(Resource):
       armour.type = args['type']
       armour.rank = args['rank']
       armour.rarity = args['rarity']
+      armour.slots = args['slots']
 
       db.session.commit()
       return armour
@@ -204,11 +273,26 @@ class Armours(Resource):
         .scalars(db.select(ArmourModel)) \
         .all()
       return result, 204
+    
+class Skills(Resource):
+  @marshal_with(skills_fields)
+  def get(self):
+    all_skills = db.session \
+      .scalars(db.select(SkillModel)) \
+      .all()
+    schema = SkillSchema(many=True)
+    return schema.dump(all_skills)
 
+
+"""
+  Endpoints
+"""
 api.add_resource(Armours, 
                  '/api/armours/', 
                  '/api/armours/<int:id>', 
                  '/api/armours/<string:slug>')
+
+api.add_resource(Skills, '/api/skills/')
 
 @app.route('/')
 def hello():
--- init_db.py
+++ init_db.py
@@ -1,22 +1,34 @@
-from api import app, db, ArmourModel, SlotModel
+from api import app, db, ArmourModel, SlotModel, SkillModel, SkillRankModel
 
 with app.app_context():
-  ar = ArmourModel(
-    name='Arkvulcan Mail \u03b2', 
-    slug='arkvulcan-mail-beta',         # slug name needs to be alpha/beta instead of unicode
-    type='chest', rank='high', rarity=8)
+  # ar = ArmourModel(
+  #   name='Arkvulcan Mail \u03b2', 
+  #   slug='arkvulcan-mail-beta',         # slug name needs to be alpha/beta instead of unicode
+  #   type='chest', rank='high', rarity=8)
 
-  slot1 = SlotModel(level=3, armour=ar)
-  slot2 = SlotModel(level=2, armour=ar)
+  # slot1 = SlotModel(level=3, armour=ar)
+  # slot2 = SlotModel(level=2, armour=ar)
   #slot3 = SlotModel(level=1, armour_model=ar)
   #slot4 = SlotModel(rank=1, armour_model=7)
   #slot5 = SlotModel(rank=2, armour_model=9)
   #slot6 = SlotModel(rank=1, armour_model=9)
 
-  db.session.add(ar)
+  sk = SkillModel(
+    name='Weakness Exploit',
+    type='Armour',
+    desc='Increases affinity of attacks that expoit a monster\'s weak points and wounds'
+  )
+
+  skr1 = SkillRankModel(level=1, desc='Attacks that hit weak points gain affinity +5%, with an extra 3% on wounds', skill=sk)
+  skr2 = SkillRankModel(level=2, desc='Attacks that hit weak points gain affinity +10%, with an extra 5% on wounds', skill=sk)
+  skr3 = SkillRankModel(level=3, desc='Attacks that hit weak points gain affinity +15%, with an extra 10% on wounds', skill=sk)
+  skr4 = SkillRankModel(level=4, desc='Attacks that hit weak points have 20% increased affinity, with an extra 15% on wounds', skill=sk)
+  skr5 = SkillRankModel(level=5, desc='Attacks that hit weak points have 30% increased affinity, with an extra 20% on wounds', skill=sk)
+
+  #print(skr1)
+  db.session.add(sk)
   #db.session.add(slot1)
-  db.session.add_all([slot1, slot2])
-  
+  db.session.add_all([skr1, skr2, skr3, skr4, skr5])
   db.session.commit()
 
 # """create new table"""
