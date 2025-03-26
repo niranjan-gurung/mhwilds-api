22dac7e4ad9d39fd1f45a018a037e04451b698e1
niranjan-gurung <gurung5130@gmail.com>
2025-03-26 20:27:01 +0000

    added flask migrations + .flaskenv.

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
+from flask_migrate import Migrate
 from config import Config
 
 db = SQLAlchemy()
 api = Api()
-# migrate = Migrate()
+migrate = Migrate()
 
 def create_app(config_class=Config):
 
@@ -14,7 +14,7 @@ def create_app(config_class=Config):
   app.config.from_object(config_class)
 
   db.init_app(app)
-  # migrate.init_app(app)
+  migrate.init_app(app, db)
 
   from .controllers import armours, skills
   api.add_resource(armours.Armours, 
