7d78cc92cc34e51a56a8ec4ccbc7ab005be75aba
niranjan-gurung <gurung5130@gmail.com>
2025-05-18 22:48:41 +0100

    charm post request implemented. Data recieved working properly!! Rarity needs fixing.

--- api/controllers/charms.py
+++ api/controllers/charms.py
@@ -1,6 +1,7 @@
-from flask import request
+from flask import request, jsonify
 from flask_restful import Resource
-from api.schemas.charm import CharmModel, CharmSchema
+from api.schemas.charm import CharmModel, CharmSchema, CharmRankModel
+from api.schemas.skill import SkillRankModel
 from api import db
 from marshmallow import ValidationError
 
@@ -21,14 +22,50 @@ class Charms(Resource):
   
   def post(self):
     json = request.get_json()
+    result, errors = [], []
     try:
-      schema = CharmSchema(session=db.session)
-      charm = schema.load(json)
+      def process_charm_item(charm_item) -> dict:
+        ranks: list = charm_item.pop('ranks', [])
+        schema = CharmSchema(session=db.session)
+        charm = schema.load(charm_item)
 
-      db.session.add(charm)
-      db.session.commit()
-      return schema.dump(charm), 201
+        db.session.add(charm)
+        db.session.flush()
 
+        # process CharmRankModel:
+        for rank in ranks:
+          skill_rank_ids = []   # stores skill ranks from SkillRankModel
+          skills = rank.pop('skills', [])
+
+          # handle 'skills' field within each rank:
+          for skill in skills:
+            skill_rank = SkillRankModel.query.get(skill['id'])
+            if skill_rank:
+              skill_rank_ids.append(skill_rank)
+
+          charm_rank = CharmRankModel(**rank, charm_id=charm.id)
+          charm_rank.skills.extend(skill_rank_ids)
+          db.session.add(charm_rank)
+
+        db.session.commit()
+        return {'id': charm.id, 'name': charm.name}
+
+      if isinstance(json, list):
+        for charm_item in json:
+          try:
+            result.append(process_charm_item(charm_item))
+          except ValidationError as err:
+            errors.append({'error': str(err), 'charm_item': charm_item})
+      else:
+        result.append(process_charm_item(json))
+      
+      if errors:
+        return {'success': result, 'errors': errors}, 207
+      return {'success': result}, 201
+   
     except ValidationError as err:
       db.session.rollback()
       return {'errors': err.messages}, 400
+    except Exception as e:
+      db.session.rollback()
+      return {'error': str(e)}, 500
